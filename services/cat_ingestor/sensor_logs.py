f7668bd8a3ee4d6085394d4fc2538c1b7fdbdaf1
Lucas Prestes Mendes <mlucasprestes@gmail.com>
2025-07-24 01:00:12 -0300

    :hammer: retries and validation in connection

--- services/cat_ingestor/sensor_logs.py
+++ services/cat_ingestor/sensor_logs.py
@@ -1,27 +1,37 @@
 import psycopg
-from dotenv import load_dotenv
-import os
+import time
 
 class SensorLogsDB:
     missing_conn = "Conexão com o banco não está estabelecida."
 
-    def __init__(self, env_path='../../../../.env'):
-        load_dotenv(env_path)
-        self.conn = self.connect()
+    def __init__(self, db_params):
+        self.db_params = db_params
+        self.conn = None
+        self.connect()
     
-    def connect(self):
-        return psycopg.connect(
-            dbname=os.getenv("POSTGRES_DB"),
-            user=os.getenv("POSTGRES_USER"),
-            password=os.getenv("POSTGRES_PASSWORD"),
-            host=os.getenv("POSTGRES_HOST", "localhost")
-        )
+    def connect(self, retries=5, delay=5):
+        for i in range(retries):
+            try:
+                self.conn = psycopg.connect(**self.db_params)
+                print("Conexão com o PostgreSQL estabelecida com sucesso.")
+                return
+            except psycopg.OperationalError as e:
+                print(f"Erro ao conectar ao PostgreSQL: {e}")
+                if i < retries - 1:
+                    print(f"Tentando novamente em {delay} segundos... ({i+1}/{retries})")
+                    time.sleep(delay)
+                else:
+                    print("Não foi possível conectar ao banco de dados após várias tentativas.")
+                    raise Exception(self.missing_conn)
+
+    @property
+    def connection(self):
+        if self.conn == None or self.conn.closed:
+            self.connect()
+        return self.conn
     
     def create_table(self):
-        if not self.conn:
-            raise Exception(self.missing_conn)
-        
-        with self.conn.cursor() as cursor:
+        with self.connection.cursor() as cursor:
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS sensor_logs (
                     id SERIAL PRIMARY KEY,
@@ -35,16 +45,10 @@ class SensorLogsDB:
             self.conn.commit()
     
     def insert_log(self, topic, message):
-        if not self.conn:
-            raise Exception(self.missing_conn)
-        
-        with self.conn.cursor() as cursor:
+        with self.connection.cursor() as cursor:
             cursor.execute("INSERT INTO sensor_logs (topic, payload) VALUES (%s, %s)", (topic, message))
             self.conn.commit()
     
     def close(self):
-        if not self.conn:
-            raise Exception(self.missing_conn)
-        
-        self.conn.close()
+        self.connection.close()
         self.conn = None
